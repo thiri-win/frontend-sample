234835f9e366cc5310457a7863f5c4550fe3f361
Thiri Win <thiriwinzhuang@gmail.com>
2022-06-14 07:41:33 +0630

    sample-2

--- sample-2/script.js
+++ sample-2/script.js
@@ -5,3 +5,8 @@ toggle.onclick=()=>navbarmenu.classList.toggle("active");
 
 const up=document.querySelector("#up");
 up.onclick=()=>window.scrollTo({top:0, behavior: "smooth"});
+
+window.onscroll=()=> {
+    if(window.scrollY >=600) up.classList.add("active");
+    else up.classList.remove("active");
+}
